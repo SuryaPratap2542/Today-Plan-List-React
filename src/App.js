7300bdbb618feebe07ddcbf1c7b755e47e552acd
sps@#254 <suryapratap2542@gmail.com>
2023-08-18 23:29:27 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import Plan from './Plan';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+ class App extends Component {
+  state={
+    items:[],
+    text:"",
+  }
+  handleChange = e =>{
+    this.setState({text: e.target.value})
+  }
+  handleAdd = e =>{
+    if (this.state.text!==""){
+      const items = [...this.state.items, this.state.text]
+      this.setState({items:items, text:""});
+    }
+  }
+  handleDelete = id =>{
+    console.log("Deleted",id); // these console code are for debug purpose
+    const OldItems = [...this.state.items]
+    console.log("OldItems",OldItems)
+    const items =OldItems.filter((element,i)=>{
+      return i!==id
+    })
+    console.log("NewItems",items)
+
+    this.setState({items:items});
+  }
+
+  render() {
+    return (
+      <div className='container-fluid my-5'>
+        <div className="row">
+          <div className="col-sm-6 mx-auto text-white text-white shadow-lg p-3">
+            <h1 className="text-center">
+              Today's Plan
+            </h1>
+            <div className="row">
+              <div className="col-9">
+                <input type="text" className='form-control' placeholder='Write your plan here' value={this.state.text} onChange={this.handleChange}/>
+              </div>
+              <div className="col-2">
+                <button className='btn btn-warning px-5 font-weight-bold' onClick={this.handleAdd}>
+                  Add
+                </button>
+              </div>
+              <div className="container-fluid">
+                <ul className="list-unstyled row m-5">
+                  {
+                    this.state.items.map((value,i)=>{
+                      return <Plan key={i} id={i} value={value} sendData={this.handleDelete}/>
+                    })
+                  }
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    )
+  }
 }
 
 export default App;
